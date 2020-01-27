ba46c5f2890904d71fa59b0bdbc63f9a43b3c11a
Bikram Sodhi <biksodhi@gmail.com>
2020-01-27 22:28:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var Kobe = []
+
+  for (let i = 0, l = musicians.length; i < l; i++){
+    Kobe.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return Kobe
+}
